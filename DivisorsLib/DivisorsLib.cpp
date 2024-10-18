4ca0dde216bcfd9a23af08933e68593ecb76afb6
Bohdan Shemet <shemebo_aehit@students.vizja.pl>
2024-10-18 15:03:36 +0200

    Improved Algorithm

--- DivisorsLib/DivisorsLib.cpp
+++ DivisorsLib/DivisorsLib.cpp
@@ -3,11 +3,17 @@
 
 std::vector<unsigned long long> divisors(unsigned long long number)
 {
-	std::vector<unsigned long long> result;
-	for (unsigned long long i = 1; i <= number; ++i)
-	{
-		if (number % i == 0)
-			result.push_back(i);
-	}
-	return result;
+    std::vector<unsigned long long> result;
+    for (unsigned long long i = 1; i * i <= number; ++i)
+    {
+        if (number % i == 0)
+        {
+            result.push_back(i);
+            if (i != number / i)
+            {
+                result.push_back(number / i);
+            }
+        }
+    }
+    return result;
 }
